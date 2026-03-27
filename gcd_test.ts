eeba329632a08ffc2dd6a31f30456b5cd72c1c62
Noël Wenger <noel_wenger@sluz.ch>
2026-03-27 09:30:18 +0100

    Implement gcdBruteForce for 4 and 2

--- gcd_test.ts
+++ gcd_test.ts
@@ -12,3 +12,15 @@ Deno.test("gcdBruteForce of 1 and 1 is 1", () => {
   // Assert
   assertEquals(result, 1);
 });
+
+Deno.test("gcdBruteForce of 4 and 2 is 2", () => {
+  // Arrange
+  const a = 4;
+  const b = 2;
+
+  // Act
+  const result = gcdBruteForce(a, b);
+
+  // Assert
+  assertEquals(result, 2);
+});
